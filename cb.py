fdf64e401bf36e2e9d6a996ac8bf81ed74a028ae
Tiren74n <44206392+Tiren74n@users.noreply.github.com>
2018-10-31 03:03:43 +0700

    Update cb.py

--- cb.py
+++ cb.py
@@ -142,7 +142,7 @@ def saring_id_group0():
 	while 1:
 		id_group = inputD('[?]Id Group')
 		tampil('\rh[*]Melihat')
-		a = buka('https://m.facebook.com/browse/group/members/?id='+id_group+'&amp;start=0&amp;listType=list_nonfriend&amp;refid=18&amp;_rdc=1&amp;_rdr')
+		a = buka('https://m.facebook.com/browse/group/members/?id='+id_group+'&amp;start=0&amp;listType=list_nonfriend&amp;refid=18&amp;_rdc=1&amp;_rdr&_rdc=1&_rdr')
 		nama = ' '.join(re.findall(r'<title>(.*?)</title>',a)[0].split()[1:])
 		try:
 			next = br.find_link(url_regex= '/browse/group/members/').url
